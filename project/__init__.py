5f3a5e84c7dbb95dcd95629c7d6eb9b33d66d5f9
Aaron M <aaron.m.manley@gmail.com>
2017-03-13 16:37:36 -0700

    base.html, invite form skeleton, modified __init__ for heroku deployment

--- project/__init__.py
+++ project/__init__.py
@@ -3,18 +3,24 @@ from flask_bcrypt import Bcrypt
 import os
 from flask import Flask
 from flask_login import LoginManager
+from flask_wtf.csrf import CsrfProtect
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
-
 db = SQLAlchemy(app)
+if os.environ.get('ENV') == 'production':
+	debug=False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or "postgres://localhost/slowcrmv2-db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
+CsrfProtect(app)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+
+from project.users import users_blueprint
+
+app.register_blueprint(users_blueprint, url_prefix='/users')
 
 
 from project.users.models import User
