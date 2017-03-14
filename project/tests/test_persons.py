61e9e46ad7b5e5d5014e516d52510f7ee1b85bf1
Aaron M <aaron.m.manley@gmail.com>
2017-03-14 15:53:59 -0700

    moved test_persons to test dir

--- project/tests/test_persons.py
+++ project/tests/test_persons.py
@@ -9,7 +9,7 @@ class BaseTestCase(TestCase):
     def create_app(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testing.db'
         app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False      
+        app.config['WTF_CSRF_ENABLED'] = False
         return app
 
     def setUp(self):
