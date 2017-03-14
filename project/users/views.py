672c291315d57c58977b33480e5de17147195364
Aaron M <aaron.m.manley@gmail.com>
2017-03-14 12:25:52 -0700

    fixed build, invoke datetime)

--- project/users/views.py
+++ project/users/views.py
@@ -5,6 +5,7 @@ from project.users.models import User, Person
 from project import db, bcrypt
 from flask_login import login_user, logout_user, current_user, login_required
 from sqlalchemy.exc import IntegrityError
+from project.users.forms import PersonForm
 
 users_blueprint = Blueprint(
     'users',
@@ -31,5 +32,22 @@ def login():
     return render_template('login.html', form=form)
 
 
-
-
+@users_blueprint.route('/persons', methods=['GET, POST'])
+def person_index():
+    form = PersonForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_person = Person(
+            email=request.form['email'],
+            phone=request.form['phone'],
+            name=request.form['name'],
+            title=request.form['title'],
+            description=request.form['description'],
+            slow_lp=request.form['slow_lp']
+                )
+            db.session.add(new_person)
+            db.session.commit()
+            flash("Succesfully added new person")
+            return redirect(url_for('users.index'))
+        flash('Please fill in all required fields')
+        return render_template('login.html', form=form)
