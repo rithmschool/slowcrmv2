3756841ddfc8a3f6c1112c127a25f4ac75439d66
tbobarino <hopmailkins@gmail.com>
2017-03-13 17:24:05 -0700

    Add Flask mail in requirements and __init__ mail setup

--- project/__init__.py
+++ project/__init__.py
@@ -3,6 +3,7 @@ from flask_bcrypt import Bcrypt
 import os
 from flask import Flask
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
@@ -12,9 +13,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or "postg
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 
 from project.users.models import User
