d4f524c42d66f8ee1d77c5d8fbf8f05c85e591b1
Aaron M <aaron.m.manley@gmail.com>
2017-03-14 10:21:59 -0700

    Person model created

--- project/users/models.py
+++ project/users/models.py
@@ -2,6 +2,10 @@ from project import db, bcrypt
 from flask_login import UserMixin
 from datetime import datetime
 
+entry_persons = db.Table('entries_persons',
+    db.Column('entry_id', db.Integer, db.ForeignKey("entries.id")),
+    db.Column('person_id',db.Integer,db.ForeignKey("persons.id"))
+    )
 
 class User(db.Model, UserMixin):
 
@@ -29,3 +33,19 @@ class User(db.Model, UserMixin):
         self.is_admin = is_admin
         self.confirmed = confirmed
 
+class Person(db.Model):
+    __tablename__ = "persons"
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.Text, nullable=True, unique=True)
+    phone = db.Column(db.Integer, nullable=True)
+    name = db.Column(db.Text, nullable=False)
+    title = db.Column(db.Text, nullable=True)
+    description = db.Column(db.Text, nullable=True)
+    slow_lp = db.Column(db.Boolean)
+    # user_created_by = db.Column(db.Integer)
+    # user_primary_poc = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+
+    
