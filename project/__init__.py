c687017eab2f562659d2fdae977207a676bbf26f
Aaron M <aaron.m.manley@gmail.com>
2017-03-14 11:01:19 -0700

    removed duplicate user_blueprint import in init.py" "

--- project/__init__.py
+++ project/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_mail import Mail
-from project.users.views import users_blueprint
+
 
 
 app = Flask(__name__)
