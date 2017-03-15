3cdb7be9442c315338042b10726e2037993d6480
tbobarino <hopmailkins@gmail.com>
2017-03-14 21:42:56 -0700

    added form for invite

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, PasswordField, BooleanField, TextAreaField
 
-
 class UserForm(FlaskForm):
 	email = StringField('Email', [validators.DataRequired()])
 	name = StringField('Name', [validators.DataRequired()])
 	password = PasswordField('Password', [validators.DataRequired()])
 	phone = StringField('Phone', [validators.DataRequired()])
-	
 
+class InviteForm(FlaskForm):
+    email = StringField('Email', [validators.DataRequired()])
+    name = StringField('Name', [validators.DataRequired()])
 
 class LoginForm(FlaskForm):
 	email = StringField('Email', [validators.DataRequired()])
 	password = PasswordField('Password', [validators.DataRequired()])
-
 
 class PersonForm(FlaskForm):
     email = StringField('Email', [validators.DataRequired()])
@@ -22,7 +22,6 @@ class PersonForm(FlaskForm):
     title = StringField('Title' , [validators.Length(max=20)])
     description = StringField('Description')
     slow_lp = BooleanField('Slow_lp')
-
 
 class EntryForm(FlaskForm):
     post_message = TextAreaField('Send an update to partners $company |person and a *tag')
