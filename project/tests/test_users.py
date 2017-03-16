c1cea2224650adb383b74e9bc732011463d81dd4
tbobarino <hopmailkins@gmail.com>
2017-03-16 12:24:45 -0700

    updated tests for login redirects

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -6,10 +6,10 @@ from project.users.token import generate_confirmation_token, confirm_token
 from project import app, db, bcrypt
 
 class BaseTestCase(TestCase):
-    def _login_user(self,email,password):
+    def _login_user(self,email,password,follow_redirects=False):
         return self.client.post('/users/login', 
             data=dict(email=email, 
-            password=password), follow_redirects=True)
+            password=password), follow_redirects=follow_redirects)
 
     def create_app(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testing.db'
@@ -34,15 +34,11 @@ class BaseTestCase(TestCase):
         self.client.get('/users/login')
         self.assert_template_used('users/login.html')
         # Wrong Password
-        response = self.client.post('/users/login', 
-            data=dict(email='tommyhopkins@gmail.com', 
-            password='wrongpassword'), follow_redirects=True)
+        response = self._login_user('tommyhopkins@gmail.com','wrongpassword',True)
         self.assertEqual(response.status_code, 200)
         self.assert_template_used('users/login.html')
         # Logging in Successfully
-        response = self.client.post('/users/login', 
-            data=dict(email='tommyhopkins@gmail.com', 
-            password='password2'), follow_redirects=True)
+        response = self._login_user('tommyhopkins@gmail.com','password2',True)
         self.assertEqual(response.status_code, 200)
         self.assert_template_used('users/home.html')      
 
