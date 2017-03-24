79a79633e86bcf4c7e916836e19a6d27b4f7b5a7
Aaron M <aaron.m.manley@gmail.com>
2017-03-24 13:11:37 -0700

    more querys to fill tag details

--- project/tags/views.py
+++ project/tags/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from project.models import Tag, Taggable, Entry, Person, Company
+from project.users.views import get_links, get_pipes_dollars_tags_tuples
 
 tags_blueprint = Blueprint(
     'tags',
@@ -11,7 +12,15 @@ tags_blueprint = Blueprint(
 def details(id):
     taggables = Taggable.query.filter(Taggable.tag_id==id).all()
     tag_name = Tag.query.get(id)
-    return render_template('tags/details.html', tag_name=tag_name, taggables=taggables, Entry=Entry, Company=Company, Person=Person)
+    tagged_entries = [ 
+    get_links(Entry.query.get(tagged.id).content,
+        get_pipes_dollars_tags_tuples(Entry.query.get(tagged.id).content)
+        ) for tagged in taggables if tagged.taggable_type == "entry"]
+
+    from IPython import embed; embed()
+    # formatted_entries = [Entry.query.get(entry.id).content for entry in taggables if entry.taggable_type=="entry"]
+    return render_template('tags/details.html', tag_name=tag_name, taggables=taggables, tagged_entries=tagged_entries, 
+        Company=Company, Person=Person)
 
 @tags_blueprint.route('/', methods=['GET'])
 def index():
