d6bdcba11a02deac71e3cc089713a53e1ef4ab4b
Aaron M <aaron.m.manley@gmail.com>
2017-03-24 10:16:57 -0700

    styling for tags page, added more queries to send to jinja template

--- project/tags/views.py
+++ project/tags/views.py
@@ -1,7 +1,5 @@
-from flask import Blueprint, redirect, render_template, flash, url_for, request
-from project import db
+from flask import Blueprint, render_template
 from project.models import Tag, Taggable, Entry, Person, Company
-from flask_login import login_required
 
 tags_blueprint = Blueprint(
     'tags',
@@ -12,7 +10,8 @@ tags_blueprint = Blueprint(
 @tags_blueprint.route('/<int:id>', methods=['GET'])
 def details(id):
     taggables = Taggable.query.filter(Taggable.tag_id==id).all()
-    return render_template('tags/details.html', taggables=taggables, Entry=Entry, Company=Company, Person=Person)
+    tag_name = Tag.query.get(id)
+    return render_template('tags/details.html', tag_name=tag_name, taggables=taggables, Entry=Entry, Company=Company, Person=Person)
 
 @tags_blueprint.route('/', methods=['GET'])
 def index():
