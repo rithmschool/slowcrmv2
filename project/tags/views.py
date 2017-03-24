b412a8a9b47b717aa84b08ad35195e27f8cd80bd
Aaron M <aaron.m.manley@gmail.com>
2017-03-24 14:42:36 -0700

    company and persons display with links, tags-added and removed small CSS changes

--- project/tags/views.py
+++ project/tags/views.py
@@ -20,12 +20,18 @@ def details(id):
         "user_name": User.query.get((Entry.query.get(tagged_entry.taggable_id).user_id)).name
         } for tagged_entry in Taggable.query.filter_by(tag_id=id, taggable_type="entry")]
 
-    formatted_companies = [get_links(Company.query.get(tagged_company.taggable_id).name,
-        get_pipes_dollars_tags_tuples(Company.query.get(tagged_company.taggable_id).name) 
-        ) for tagged_company in Taggable.query.filter_by(tag_id=id, taggable_type="company")]
-    from IPython import embed; embed()
-    # formatted_entries = [Entry.query.get(entry.id).content for entry in taggables if entry.taggable_type=="entry"]
-    return render_template('tags/details.html', tag_name=tag_name, formatted_entries=formatted_entries, formatted_companies=formatted_companies)
+    formatted_companies = [{
+        "company_name": Company.query.get(company.taggable_id).name,
+        "company_id": Company.query.get(company.taggable_id).id,
+        "tags": [Tag.query.get(tag.tag_id) for tag in Taggable.query.filter_by(taggable_id=Company.query.get(company.taggable_id).id, taggable_type="company")]
+        } for company in Taggable.query.filter_by(tag_id=id, taggable_type="company")]
+
+    formatted_persons = [{
+        "person_name": Person.query.get(person.taggable_id).name,
+        "person_id": Person.query.get(person.taggable_id).id,
+         "tags": [Tag.query.get(tag.tag_id) for tag in Taggable.query.filter_by(taggable_id=Person.query.get(person.taggable_id).id, taggable_type="person")]
+        } for person in Taggable.query.filter_by(tag_id=id, taggable_type="person")]
+    return render_template('tags/details.html', tag_name=tag_name, formatted_entries=formatted_entries, formatted_companies=formatted_companies, formatted_persons=formatted_persons)
 
 @tags_blueprint.route('/', methods=['GET'])
 def index():
