0b67659e16274a911103027280108462333b0cf6
Aaron M <aaron.m.manley@gmail.com>
2017-03-24 13:51:19 -0700

    entries rendering properly on tag page, removed unneeded CSS

--- project/tags/views.py
+++ project/tags/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from project.models import Tag, Taggable, Entry, Person, Company
+from project.models import Tag, Taggable, Entry, Person, Company, User
 from project.users.views import get_links, get_pipes_dollars_tags_tuples
 
 tags_blueprint = Blueprint(
@@ -10,17 +10,22 @@ tags_blueprint = Blueprint(
 
 @tags_blueprint.route('/<int:id>', methods=['GET'])
 def details(id):
-    taggables = Taggable.query.filter(Taggable.tag_id==id).all()
+    # taggables = Taggable.query.filter(Taggable.tag_id==id).all()
     tag_name = Tag.query.get(id)
-    tagged_entries = [ 
-    get_links(Entry.query.get(tagged.id).content,
-        get_pipes_dollars_tags_tuples(Entry.query.get(tagged.id).content)
-        ) for tagged in taggables if tagged.taggable_type == "entry"]
+    formatted_entries = [{
+        "content": get_links(Entry.query.get(tagged_entry.taggable_id).content,
+        get_pipes_dollars_tags_tuples(Entry.query.get(tagged_entry.taggable_id).content) 
+        ),
+        "user_id": User.query.get((Entry.query.get(tagged_entry.taggable_id).user_id)).id,
+        "user_name": User.query.get((Entry.query.get(tagged_entry.taggable_id).user_id)).name
+        } for tagged_entry in Taggable.query.filter_by(tag_id=id, taggable_type="entry")]
 
+    formatted_companies = [get_links(Company.query.get(tagged_company.taggable_id).name,
+        get_pipes_dollars_tags_tuples(Company.query.get(tagged_company.taggable_id).name) 
+        ) for tagged_company in Taggable.query.filter_by(tag_id=id, taggable_type="company")]
     from IPython import embed; embed()
     # formatted_entries = [Entry.query.get(entry.id).content for entry in taggables if entry.taggable_type=="entry"]
-    return render_template('tags/details.html', tag_name=tag_name, taggables=taggables, tagged_entries=tagged_entries, 
-        Company=Company, Person=Person)
+    return render_template('tags/details.html', tag_name=tag_name, formatted_entries=formatted_entries, formatted_companies=formatted_companies)
 
 @tags_blueprint.route('/', methods=['GET'])
 def index():
