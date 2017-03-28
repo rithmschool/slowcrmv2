f31a3c1ade43055d3787fc1ccc7efb781586497f
tostaylo <tostaylo1@gmail.com>
2017-03-28 12:06:49 -0700

    Search functionality for multiple search terms
    Ability to search for multiple search terms simultaneously in Search feature

--- project/users/views.py
+++ project/users/views.py
@@ -30,15 +30,35 @@ def home():
 @users_blueprint.route('/search', methods=['GET'])
 @login_required
 def search():
-    term = request.args.get('search')
-    tag_exists = bool(Tag.query.filter_by(text=term).first())
-    company_exists = bool(Company.query.filter_by(name=term).first())
-    person_exists = bool(Person.query.filter_by(name=term).first())
-    entry_exact = Entry.query.filter(Entry.content.ilike("%{}%".format(term)))
-    person_exact = Person.query.filter(Person.name.ilike("%{}%".format(term)))
-    company_exact = Company.query.filter(Company.name.ilike("%{}%".format(term)))
-    tag_exact = Tag.query.filter(Tag.text.ilike("%{}%".format(term)))
-    count = max(entry_exact.count(), person_exact.count(), company_exact.count(), tag_exact.count())
+    search_terms = request.args.get('search').split(" ")
+    entry = []
+    person = []
+    company = []
+    tag = []
+    entry_exists = bool
+    tag_exists = bool
+    company_exists = bool
+    person_exists = bool
+    for term in search_terms:
+        if len(term) >= 3:
+            tag_exists = bool(Tag.query.filter(Tag.text.contains(term)).first())
+            if tag_exists:
+                tag.append(Tag.query.filter(Tag.text.contains(term)))
+            company_exists = bool(Company.query.filter(Company.name.contains(term)).first())
+            if company_exists:
+                company.append(Company.query.filter(Company.name.contains(term)))
+            person_exists = bool(Person.query.filter(Person.name.contains(term)).first())
+            if person_exists:
+                person.append(Person.query.filter(Person.name.contains(term)))
+            entry_exists = bool(Entry.query.filter(Entry.content.contains(term)).first())
+            if entry_exists:
+                entry.append(Entry.query.filter(Entry.content.contains(term)))
+
+    tag_exact = [item for sublist in tag for item in sublist]
+    person_exact = [item for sublist in person for item in sublist]
+    entry_exact = [item for sublist in entry for item in sublist]
+    company_exact = [item for sublist in company for item in sublist]
+    count = len(tag_exact) + len(person_exact) + len(company_exact) + len(entry_exact)
     return render_template('users/search.html', entry_exact=entry_exact, person_exact=person_exact, company_exact=company_exact,
         tag_exact=tag_exact, count=count, term=term, get_links=get_links, get_pipes_dollars_tags_tuples=get_pipes_dollars_tags_tuples,
         tag_exists=tag_exists, company_exists=company_exists, person_exists=person_exists)
