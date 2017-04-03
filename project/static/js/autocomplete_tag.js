07e2a8991583c226f96ed1984265962d76e52321
Joel <japoncea@gmail.com>
2017-04-03 16:53:22 -0700

    refactoring code to clean up indentation

--- project/static/js/autocomplete_tag.js
+++ project/static/js/autocomplete_tag.js
@@ -26,27 +26,27 @@ $(function(){
 
 
 $(function(){
-    var sendServer = false;
-    var lookupResults;
-    $('#tag').autocomplete({
-        ajaxSettings: {
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader('X-Requested-With', 'XMLHTTPRequest');
-            }
-        },
-        onSearchStart: function(params) {
-            return true;
-        },
-        lookup: function(query, done){
-            $.ajax({
-                url: '/persons/tags/autocomplete',
-                data: {
-                    params: query
-                }
-            }).then(function(response){
-                    lookupResults = jQuery.parseJSON(response);
-                    done(lookupResults);
-            });
-        },
-    });
+  var sendServer = false;
+  var lookupResults;
+  $('#tag').autocomplete({
+    ajaxSettings: {
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('X-Requested-With', 'XMLHTTPRequest');
+      }
+    },
+    onSearchStart: function(params) {
+      return true;
+    },
+    lookup: function(query, done){
+      $.ajax({
+        url: '/persons/tags/autocomplete',
+        data: {
+          params: query
+        }
+      }).then(function(response){
+        lookupResults = jQuery.parseJSON(response);
+        done(lookupResults);
+      });
+    },
+  });
 });
