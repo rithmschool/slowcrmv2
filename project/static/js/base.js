0c0a476ef0d176b2309a9d03840baabd7ea33c6c
Joel <japoncea@gmail.com>
2017-04-07 16:38:51 -0700

    updated styling for users, persons show
    entries are now better aligned on page and have individual containers
    cleaned up whitespace in code

--- project/static/js/base.js
+++ project/static/js/base.js
@@ -1,55 +1,54 @@
 $(function() {
+  // MODAL FOCUS
+  $('.modal').on('shown.bs.modal', function() {
+    $(this).find('[autofocus]').focus();
+  });
 
-    // MODAL FOCUS
-    $('.modal').on('shown.bs.modal', function() {
-        $(this).find('[autofocus]').focus();
-    });
+  // MODAL CLEAR ON HIDE
+  $(".modal").on("hidden.bs.modal", function(){
+    $("#tweet-message").val("");
+  });
 
-    // MODAL CLEAR ON HIDE
-    $(".modal").on("hidden.bs.modal", function(){
-        $("#tweet-message").val("");
-    });
+  //TAG BUTTONS//
+  $('.tags_select span').click(function() {
+    var value = $(this).text()[0];
+    var input = $('#tweet-message');
+    input.focus();
+    input.val(input.val() + value + value);
+    var position = input.val().length-1;
+    input[0].setSelectionRange(position,position);
+    return false;
+   });
 
-    //TAG BUTTONS//
-    $('.tags_select span').click(function() {
-         var value = $(this).text()[0];
-         var input = $('#tweet-message');
-         input.focus();
-         input.val(input.val() + value + value);
-         var position = input.val().length-1;
-         input[0].setSelectionRange(position,position);
-         return false;
-     });
+  let $content = $('.content');
+  let $searchContent = $('.searchcontent');
+  let $modalMessage = $('#modal-message')
 
-    let $content = $('.content');
-    let $searchContent = $('.searchcontent');
-    let $modalMessage = $('#modal-message')
-
-    // Invite User Modal Form
-    $("#invite-modal-submit").on('click', function(e) {
-        e.preventDefault();
-        input.focus();
-        $modalMessage.html('<p>One moment...</p>');
-        var email = $("#invite-email").val();
-        var name = $("#invite-name").val();
-        $.ajax({
-            type: "POST",
-            url: "/users/invite",
-            dataType: "json",
-            contentType: "application/json",
-            data: JSON.stringify({ name, email })
-        }).then(function(response) {
-            function sent(){
-              $modalMessage.html('<p></p>');
-              $("#invite-modal").modal('toggle');
-            }
-            $modalMessage.html('<p style="color:green">Successfully sent invite!</p>');
-            $(".invite-modal-form").trigger('reset');
-            setTimeout(sent, 2000)
-        }).catch(function(e) {
-            if (JSON.parse(e.responseText) === "Missing form info") {
-                $modalMessage.html('<span style="color:red">Please fill in name and email</span>');
-            }
-        })
-    });
+  // Invite User Modal Form
+  $("#invite-modal-submit").on('click', function(e) {
+    e.preventDefault();
+    input.focus();
+    $modalMessage.html('<p>One moment...</p>');
+    var email = $("#invite-email").val();
+    var name = $("#invite-name").val();
+    $.ajax({
+      type: "POST",
+      url: "/users/invite",
+      dataType: "json",
+      contentType: "application/json",
+      data: JSON.stringify({ name, email })
+    }).then(function(response) {
+      function sent(){
+        $modalMessage.html('<p></p>');
+        $("#invite-modal").modal('toggle');
+      }
+      $modalMessage.html('<p style="color:green">Successfully sent invite!</p>');
+      $(".invite-modal-form").trigger('reset');
+      setTimeout(sent, 2000)
+    }).catch(function(e) {
+      if (JSON.parse(e.responseText) === "Missing form info") {
+          $modalMessage.html('<span style="color:red">Please fill in name and email</span>');
+      }
+    })
+  });
 });
